cc85cc8f996b5229823226a1353b5e457a42ed15
Hoshang Charania <hok14@pitt.edu>
2018-12-01 21:19:02 -0500

    Initial commit to Blood Sugar Calculator

---
+++ README.md
@@ -0,0 +1,8 @@
+## Blood Glucose Indicator
+
+A basic Glucose monitor which checks the blood glucose level.
+
+## Website
+
+https://hoshangcharania.github.io/BloodGlucoseLevel/
+
---
+++ js/sugarcheck.js
@@ -0,0 +1,53 @@
+angular.module('HealthApp', [])
+    .controller('HealthController', function($scope) {
+        
+        $scope.deleteAll = function(){
+           afterOp=0.0;
+           f_value=0.0;
+           first_entry=true;
+           old_value_op='add';
+           $scope.value = null;
+        }
+        
+        $scope.updateOutput =function(number){
+            if(($scope.value == null) || ($scope.value == undefined)){
+                $scope.value="";
+            }
+            if(typeof number=="number" || number=="."){
+            number=number.toString();
+            $scope.value =$scope.value+number;
+            $scope.widget = {title: $scope.value};
+        }else if(number==null){
+            $scope.value =$scope.widget.title;
+        }
+    }
+
+    $scope.checkValid = function(){
+        number=Number($scope.value);
+        if((typeof number=="number") & (number>=0) & (number.toString().length<=20)){
+            // do computation
+            $scope.checkPeriod(number);
+        }else{
+            $scope.classification="";
+            $('.parent').empty();
+            $('.parent').append("<div class='alert alert-danger alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Please Enter a Valid Input.</div>")
+            $(".parent").fadeTo(2000, 500).slideUp(2000, function(){
+                $(".parent").slideUp(2000);
+                 });   
+        }
+    }
+
+    $scope.checkPeriod= function(number){
+        if(number<=69){
+            $scope.classification= $scope.value+"mg/dL is LOW";
+        }else if((number>=70) & (number<=110)){
+            $scope.classification= $scope.value+"mg/dL is NORMAL";
+        }else if((number>=111) & (number<=125)){
+            $scope.classification= $scope.value+"mg/dL is PRE-DIABETES";
+        }else if((number>=125)){
+            $scope.classification= $scope.value+"mg/dL is DIABETES";
+        }
+        }
+
+    });
+
